5ee4578d3c1496d17b95027f7a02faa48f0b6bb0
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-05-19 05:55:41 +0530

    server index.js imports

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
+
+const app = express();
+app.use(bodyParser.json({ limit:"30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
+
+
+
+const PORT = process.env.PORT || PORT;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
