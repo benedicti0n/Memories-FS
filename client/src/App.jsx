5f17eb5f099dc0ce315a42000b04448b11f6c157
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-06-16 13:44:17 +0530

    frontend replaced with vite and components add, MUI add

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+    <h1>hi</h1>
+    </>
+  )
+}
+
+export default App
